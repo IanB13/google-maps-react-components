ed10a38d17cdb88abbe6bbff7bc754cd04ec61aa
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-11-21 23:41:27 +0000

    updated marker component

--- src/components/marker/Marker.jsx
+++ src/components/marker/Marker.jsx
@@ -4,26 +4,26 @@ import {MapContext} from '../../state/mapState'
 import cleanup from '../infoWindow/cleanup'
 import InfoWindow from '../infoWindow/InfoWindow'
 
-/*options, maybe do typeScript for only this file 
-options ={
-    icon: svg, // Icon svg
-    content: html string // for what to put into the information window
-    type: string // ID for delete and direction operations
-    loading: bool // 
-    info: object // Specific for this one, 
-}
-*/
 
-const Marker = ({ postion, options, markerKey, children }) => {
+//MarkerOptions
+// https://developers.google.com/maps/documentation/javascript/reference/marker#MarkerOptions
 
-    //gets google state from redux store, updates 
+//TODO: figure out better options and marker options naming
+const Marker = ({ postion, type, markerKey, markerOptions, children }) => {
+
+    //gets google state from store, updates 
     const {state,dispatch} = useContext(MapContext)
     const google = state.google
 
 
+
     //useEffect add and deletes map markers on re-render
    useEffect(() => {  
-        const marker = createMarker(google,postion, markerKey,options)
+       //allows use of google in options
+       if (typeof (markerOptions) === 'function') {
+           markerOptions = markerOptions(google)
+       }
+        const marker = createMarker(google,postion, markerKey,type, markerOptions)
         dispatch({type: "ADD_MARKER", data: marker})
         
         return (() => {
@@ -32,10 +32,11 @@ const Marker = ({ postion, options, markerKey, children }) => {
             dispatch({type: "CLEAN_UP"})
         })
     
-    //doesn't have state.cleanup included, adding this would cause premature deletion
-    // of infowindow dom nodes
+    /* state.cleanup is not included in useEffect dependancy array, 
+    would cause useEffect to trigger too much causing premature 
+    deletion of infowindow dom nodes */
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [dispatch,google,markerKey,options,postion]) 
+    }, [dispatch,google,markerKey,type,postion ,markerOptions]) 
 
     if(children){
     return (
